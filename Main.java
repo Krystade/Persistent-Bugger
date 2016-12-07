af0eb9cfbe08722b51ce308ba70324035407ea10
Krys <jackpschmid@gmail.com>
2016-12-07 15:39:11 -0800

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,13 @@
+class Persist {
+	public static int persistence(long n) {
+    int test = 1;
+    int count = 0;
+    while (){
+  		while (test < n){
+        test *= 10;
+        count++;
+      }
+    }
+  int remainder = n % 10;
+	}
+}
